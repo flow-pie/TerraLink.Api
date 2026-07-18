06726ba3c5b548d6f174f3e03830d56765df0ad6
flow-pie <startabase@gmail.com>
2026-07-18 15:04:20 +0300

    feat: Implement UserDto and add user-related endpoints; remove unused HTTP file

---
+++ TerrraLink.Api/DTOs/UserDto.cs
@@ -0,0 +1,19 @@
+namespace TerrraLink.Api.DTOs;
+
+//A DTO (Data Transfer Object) is a simple object that is used to transfer data between different layers of an application. 
+//It typically contains only the data that needs to be transferred, without any business logic or behavior.
+public record UserDto(
+    long Id,
+    string? EmployeeId,
+    string FullName,
+    string? Email,
+    string Phone,
+    string Password,
+    string Role,
+    string Status,
+    bool MfaEnabled,
+    string MfaSecret,
+    DateTime? LastLogin,
+    DateTime CreatedAt,
+    DateTime? UpdatedAt
+);
--- TerrraLink.Api/Program.cs
+++ TerrraLink.Api/Program.cs
@@ -1,3 +1,5 @@
+using TerrraLink.Api.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,16 +10,59 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// // Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.MapOpenApi();
+// }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 
-app.UseAuthorization();
+// app.UseAuthorization();
 
-app.MapControllers();
+// app.MapControllers();
+
+List<UserDto> users = [
+    new(
+        1,
+        "TL-001",
+        "Josto",
+        "josto@example.com",
+        "123-456-7890",
+        "password123",
+        "User",
+        "Active",
+        false,
+        "",
+        null,
+        DateTime.UtcNow,
+        null
+    ),
+    new(
+        2,
+        "TL-002",
+        "Alice",
+        "alice@example.com",
+        "987-654-3210",
+        "password456",
+        "Admin",
+        "Active",
+        true,
+        "MFASecret123",
+        DateTime.UtcNow.AddDays(-1),
+        DateTime.UtcNow,
+        DateTime.UtcNow.AddDays(-1)
+    ),
+];
+
+//GET /
+app.MapGet("/", () => "Hello World!");
+
+//GET /users
+app.MapGet("/users", ()=> users);
+
+//GET /users/{id}
+app.MapGet("/users/{id}", (long id)=> users.Find(user => user.Id==id) );
+
 
 app.Run();
