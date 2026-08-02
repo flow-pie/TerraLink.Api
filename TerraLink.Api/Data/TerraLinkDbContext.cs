7d735468f00dd52bf27028d5baefbaaf69cfbf9a
flow-pie <startabase@gmail.com>
2026-08-02 12:36:05 +0300

    feat: Add additional DbSet properties to TerraLinkDbContext for CreditHistory, KycDocument, Branch, Group, LoanProduct, LoanApplication, Loan, IncomeAssessment, RepaymentSchedule, Payment, Disbursment, LoanClosure, Notification, ReportSchedule, and AuditLog models

--- TerraLink.Api/Data/TerraLinkDbContext.cs
+++ TerraLink.Api/Data/TerraLinkDbContext.cs
@@ -2,11 +2,28 @@ using Microsoft.EntityFrameworkCore;
 
 namespace TerraLink.Api.Data;
 
+//Applied via Db Migrations
 public class TerraLinkDbContext(DbContextOptions<TerraLinkDbContext> options) : DbContext(options)
 {
     public DbSet<Models.User> Users => Set<Models.User>();
     public DbSet<Models.Role> Roles => Set<Models.Role>();
     public DbSet<Models.Client> Clients => Set<Models.Client>();
+    public DbSet<Models.CreditHistory> CreditHistories => Set<Models.CreditHistory>();
+    public DbSet<Models.KycDocument> KycDocuments => Set<Models.KycDocument>();
+    public DbSet<Models.Branch> Branches => Set<Models.Branch>();
+    public DbSet<Models.Group> Groups => Set<Models.Group>();
+    public DbSet<Models.LoanProduct> LoanProducts => Set<Models.LoanProduct>();
+    public DbSet<Models.LoanApplication> LoanApplications => Set<Models.LoanApplication>();
+    public DbSet<Models.Loan> Loans => Set<Models.Loan>();
+    public DbSet<Models.IncomeAssessment> IncomeAssessments => Set<Models.IncomeAssessment>();
+    public DbSet<Models.RepaymentSchedule> RepaymentSchedules => Set<Models.RepaymentSchedule>();
+    public DbSet<Models.Payment> Payments => Set<Models.Payment>();
+    public DbSet<Models.Disbursment> Disbursments => Set<Models.Disbursment>();
+    public DbSet<Models.LoanClosure> LoanClosures => Set<Models.LoanClosure>();
+    public DbSet<Models.Notification> Notifications => Set<Models.Notification>();
+    public DbSet<Models.ReportSchedule> ReportSchedules => Set<Models.ReportSchedule>();
+    public DbSet<Models.ReportSchedule> Reports => Set<Models.ReportSchedule>();
+    public DbSet<Models.AuditLog> AuditLogs => Set<Models.AuditLog>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
